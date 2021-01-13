7244dda795c1f9cf103347a2a0685b5b76908a84
hanbee <sonhanbi1002@gmail.com>
2021-01-13 14:51:33 +0900

    지연 로딩 및 영속성 전이 설정 추가

--- src/main/java/sample/jpa/helloshop/domain/Order.java
+++ src/main/java/sample/jpa/helloshop/domain/Order.java
@@ -18,14 +18,14 @@ public class Order extends BaseEntity {
     @Column(name = "ORDER_ID")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "MEMBER_ID")
     private Member member;  // 주문 회원
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<OrderItem>();
 
-    @OneToOne
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JoinColumn(name = "DELIVERY_ID")
     private Delivery delivery;  // 배송 정보
 
--- src/main/java/sample/jpa/helloshop/domain/OrderItem.java
+++ src/main/java/sample/jpa/helloshop/domain/OrderItem.java
@@ -15,11 +15,11 @@ public class OrderItem extends BaseEntity {
     @Column(name = "ORDER_ITEM_ID")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "ORDER_ID")
     private Order order;  // 주문
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "ITEM_ID")
     private Item item;  // 주문 상품
 
