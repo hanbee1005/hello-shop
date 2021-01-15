34f0818dd82e37fefdcc5863a6ac59a1634bc1e6
hanbee <sonhanbi1002@gmail.com>
2021-01-15 16:05:13 +0900

    상품 등록하기

--- src/main/java/sample/jpa/helloshop/controller/ItemController.java
+++ src/main/java/sample/jpa/helloshop/controller/ItemController.java
@@ -2,10 +2,14 @@ package sample.jpa.helloshop.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import sample.jpa.helloshop.domain.item.Book;
+import sample.jpa.helloshop.domain.item.Item;
 import sample.jpa.helloshop.service.ItemService;
+
+import java.util.List;
 
 @Controller
 public class ItemController {
@@ -17,6 +21,13 @@ public class ItemController {
         this.itemService = itemService;
     }
 
+    @GetMapping("/items")
+    public String list(Model model) {
+        List<Item> items = itemService.findItems();
+        model.addAttribute("items", items);
+        return "items/itemList";
+    }
+
     @GetMapping("/items/new")
     public String createForm() {
         return "items/createItemForm";
@@ -25,6 +36,6 @@ public class ItemController {
     @PostMapping("/items/new")
     public String create(Book item) {
         itemService.saveItem(item);
-        return "redirect:/";
+        return "redirect:/items";
     }
 }
