ffd24fac00ec1a361c530beffadea809868b4f1a
hanbee <sonhanbi1002@gmail.com>
2021-01-15 16:24:13 +0900

    상품 업데이트 화면 구현

--- src/main/java/sample/jpa/helloshop/controller/ItemController.java
+++ src/main/java/sample/jpa/helloshop/controller/ItemController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import sample.jpa.helloshop.domain.item.Book;
 import sample.jpa.helloshop.domain.item.Item;
@@ -21,6 +23,11 @@ public class ItemController {
         this.itemService = itemService;
     }
 
+    /**
+     * 상품 조회
+     * @param model
+     * @return
+     */
     @GetMapping("/items")
     public String list(Model model) {
         List<Item> items = itemService.findItems();
@@ -28,6 +35,10 @@ public class ItemController {
         return "items/itemList";
     }
 
+    /**
+     * 상품 등록
+     * @return
+     */
     @GetMapping("/items/new")
     public String createForm() {
         return "items/createItemForm";
@@ -35,6 +46,19 @@ public class ItemController {
 
     @PostMapping("/items/new")
     public String create(Book item) {
+        itemService.saveItem(item);
+        return "redirect:/items";
+    }
+
+    @GetMapping("items/{itemId}/edit")
+    public String updateItemForm(@PathVariable("itemId") Long itemId, Model model) {
+        Item item = itemService.findOne(itemId);
+        model.addAttribute("item", item);
+        return "items/updateItemForm";
+    }
+
+    @PostMapping("items/{itemId}/edit")
+    public String updateItem(@ModelAttribute("item") Book item) {
         itemService.saveItem(item);
         return "redirect:/items";
     }
