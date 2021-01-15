c0b9bffbcf918abeef390e3289f4cd596300fc11
hanbee <sonhanbi1002@gmail.com>
2021-01-15 15:22:27 +0900

    홈 화면 구성

---
+++ src/main/java/sample/jpa/helloshop/controller/HomeController.java
@@ -0,0 +1,12 @@
+package sample.jpa.helloshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String Home() {
+        return "home";
+    }
+}
