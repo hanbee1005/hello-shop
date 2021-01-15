6ee42581a5942dd0bea8148ed996f08dd82e6e18
hanbee <sonhanbi1002@gmail.com>
2021-01-15 15:51:28 +0900

    상품 등록 화면 구현

---
+++ src/main/java/sample/jpa/helloshop/controller/ItemController.java
@@ -0,0 +1,30 @@
+package sample.jpa.helloshop.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import sample.jpa.helloshop.domain.item.Book;
+import sample.jpa.helloshop.service.ItemService;
+
+@Controller
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @Autowired
+    public ItemController(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
+    @GetMapping("/items/new")
+    public String createForm() {
+        return "items/createItemForm";
+    }
+
+    @PostMapping("/items/new")
+    public String create(Book item) {
+        itemService.saveItem(item);
+        return "redirect:/";
+    }
+}
